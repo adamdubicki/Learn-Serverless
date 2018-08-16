fdd49cbc05dfe3727580aef6c1b4b9a06934e60d
adamdubicki <adam@freshworks.io>
2018-08-16 11:21:21 -0700

    finished yelp scraper

--- yelp-scraper/handler.js
+++ yelp-scraper/handler.js
@@ -1,22 +1,28 @@
 'use strict';
-const { getPage, parsePage, saveRatingsToDB } = require('./utils');
+const { getPage, parsePage, saveRatingsToDB, deployScrapers } = require('./utils');
 
 module.exports.scrape = (event, context, callback) => {
 
   getPage(event)
     .then(page => parsePage(page))
-    .then(yelpData => saveRatingsToDB(yelpData));
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    .then(yelpData => saveRatingsToDB(yelpData, event))
+    .then(() => callback(null, {
+      statusCode: 200,
+        body: JSON.stringify({
+          message: `Scraped ${event}`,
+        })
+    }))
+    .catch(error => callback(new Error(`Error scraping ${event}: ${JSON.stringify(error)}`)));
 };
+
+module.exports.launch_scrapers = (event, context, callback) => {
+  const fakeDatabaseResults = [
+    "urban-light-at-lacma-los-angeles",
+    "the-museum-of-contemporary-art-los-angeles",
+    "the-last-bookstore-los-angeles"
+  ];
+
+  fakeDatabaseResults.forEach(businessName => {
+    deployScrapers(businessName)
+  })
+}
--- yelp-scraper/utils/saveRatingsToDB.js
+++ yelp-scraper/utils/saveRatingsToDB.js
@@ -1,3 +1,27 @@
-module.exports = (yelpData) => {
-  console.log(yelpData);
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (yelpData, businessName) => {
+  const date = JSON.stringify(new Date());
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      businessName: businessName,
+      reviewCount: yelpData.reviewCount,
+      rating: yelpData.rating,
+      scrapedAt: date
+    }
+  }
+
+  dynamoDB.put(params, error => {
+    if(error){
+      console.error(`Error saving data to DynamoDB: ${JSON.stringify(error)}`);
+      return Promise.reject(`Error saving data to DynamoDB: ${JSON.stringify(error)}`);
+    } else {
+      return Promise.resolve(params.item);
+    }
+  });
 };
